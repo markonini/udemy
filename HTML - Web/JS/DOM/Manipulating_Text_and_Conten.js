03c5375aec1978e1b22864284625690d5d690eb3
Marko Nisavic <a.marko.nisavic@tlscontact.com>
2018-11-30 14:53:27 +0100

    181

--- HTML - Web/JS/DOM/Manipulating_Text_and_Conten.js
+++ HTML - Web/JS/DOM/Manipulating_Text_and_Conten.js
@@ -13,3 +13,9 @@ tag.innerHTML   //"Corgi mixes are <strong>super</strong> adorable"
 
 // In-line change
 document.querySelector("h1").textContent = "End of this lession!";
+
+// What is big change between innerHTML and textContent also. If you want to change some text and for text you put like "<h1>Goodbye</h1>"
+// and you use .textContent is will display all text with <h1>
+// But if you use .innerHTML he will only show text and applye it with h1 style.
+document.body.textContent = "<h1>Goodbye</h1>"; //It will dispaly text on page like <h1>Goodbye</h1>
+document.body.innerHTML = "<h1>goodbye</h1>"; //it will show only Goodbye
