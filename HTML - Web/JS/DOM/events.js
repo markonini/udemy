04714187e62c71278be390c8d4d9800f6ab92fe8
Marko Nisavic <a.marko.nisavic@tlscontact.com>
2018-12-03 11:19:04 +0100

    185

---
+++ HTML - Web/JS/DOM/events.js
@@ -0,0 +1,8 @@
+// The Syntax
+Element.addEventListener(type, function);
+// Example
+var button = document.querySelector("button");
+button.addEventListener("click",function() {
+    console.log("someone clicked the button!");
+});
+
