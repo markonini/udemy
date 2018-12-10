a53c8af3a9e76ef365bcb2cd6315397d7ba03e4b
Marko Nisavic <a.marko.nisavic@tlscontact.com>
2018-12-10 16:44:21 +0100

    209

---
+++ HTML - Web/JS/jQuery/209-methods-js.js
@@ -0,0 +1,15 @@
+$("h1").text();
+
+$("ul").text();
+
+$("li").text();
+
+$("li").text("new text");
+
+$("li").text("Rusty, Colt's dog is adorable!");
+
+// html
+$("ul").html();
+
+$("ul").html("<li>I hacked your </ul><li>Rasty is still adorable</ul>");
+$("li").html('<a href="google.com">Click me to go to google</a>');
