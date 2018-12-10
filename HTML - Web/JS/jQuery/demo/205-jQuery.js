36688a562201a2a7033ac26f7aa2c4769a4d44f6
Marko Nisavic <a.marko.nisavic@tlscontact.com>
2018-12-10 13:36:02 +0100

    .

--- HTML - Web/JS/jQuery/demo/205-jQuery.js
+++ HTML - Web/JS/jQuery/demo/205-jQuery.js
@@ -1,5 +1,5 @@
 // select elem with id "special" and give it a border
-$("#special").css.("border", "2px solid red");
+$("#special").css("border", "2px solid red");
 
 // we can aslo pass in an object with styles
 var styles = {
@@ -8,3 +8,28 @@ var styles = {
 };
 
 $("#special").css(styles);
+
+// Manipulation style
+// .css(property, value)
+$("li").css("color", "yellow");
+
+// select all elements with class "big"
+// and give them an orange border
+$(".big").css("border", "1px dashed orange");
+
+//              Example to change color for few "li" with
+// DOM it goes like:
+var lis = document.querySelectorAll("li");
+for( var i = 0; i < lis.length; i++) {
+    lis[i].style.color = "green"
+};
+
+// and with jQuery is like:
+$("li").css("color", "red");
+
+//              Example
+$("li").css({
+    fontSize: "10px",
+    border: "3px dashed purple",
+    backgroundColor: "rgba(89, 45, 20, 0.5)"
+});
